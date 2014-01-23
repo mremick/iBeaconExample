c91610a88adbef0c20e56ae331d29f44c4f69265
Matt Remick <mremick@gmail.com>
2014-01-22 18:16:40 -0800

    Initial Commit

---
+++ iBeacon/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iBeacon
+//
+//  Created by Matt Remick on 1/22/14.
+//  Copyright (c) 2014 Matt Remick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iBeacon/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iBeacon
+//
+//  Created by Matt Remick on 1/22/14.
+//  Copyright (c) 2014 Matt Remick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
